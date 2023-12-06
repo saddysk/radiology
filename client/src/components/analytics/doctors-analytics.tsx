51fa290b0750aac1afe95fa546338b2a48a935f6
saddysk <sksaddamh08@gmail.com>
2023-12-07 02:05:37 +0530

    update: date filter

--- client/src/components/analytics/doctors-analytics.tsx
+++ client/src/components/analytics/doctors-analytics.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import {
-  useGetDoctorAnalytics,
-  useUserData,
-  useUserDetailData,
-} from "@/lib/query-hooks";
-import Nabvbar from "../navbar";
-import { Card } from "../ui/card";
 import * as React from "react";
 import {
   ColumnDef,
@@ -20,19 +13,8 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import {
   Table,
@@ -43,12 +25,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { BookingDto } from "@/app/api/data-contracts";
-import {
-  amount,
-  convertAgeFromMonthsToYears,
-  downloadCSV,
-  toTitleCase,
-} from "@/lib/utils";
+import { amount, downloadCSV, toTitleCase } from "@/lib/utils";
 import { DatePickerWithRange } from "../ui/date-range-picker";
 import { addDays } from "date-fns";
 import { DateRange } from "react-day-picker";
@@ -68,70 +45,99 @@ type Doctor = {
 
 type Data = Doctor[];
 
+function formatOutputToDto(output: BookingDto[], dateRange: DateRange) {
+  if (!output) {
+    return;
+  }
+
+  // let filterDataByDate = output;
+
+  // const startDate = dateRange?.from;
+  // const endDate = dateRange?.to;
+  // if (startDate != null && endDate != null) {
+  //   filterDataByDate = output.filter((item) => {
+  //     const itemDate = new Date(item.createdAt);
+  //     return itemDate >= startDate && itemDate <= endDate;
+  //   });
+  // }
+
+  const groupedByDoctor: any = {};
+
+  for (const item of output) {
+    const doctorId = item.consultant;
+
+    if (!groupedByDoctor[doctorId]) {
+      groupedByDoctor[doctorId] = {
+        name: item.consultantName,
+        id: doctorId,
+        modality: [],
+      };
+    }
+
+    const referralAmount = item.referralAmount ? item.referralAmount : 0;
+    const totalAmount = item.totalAmount ? item.totalAmount : 0;
+
+    let modalityEntry = groupedByDoctor[doctorId].modality.find(
+      (m: any) => m.name === item.modality
+    );
+
+    if (!modalityEntry) {
+      modalityEntry = {
+        name: item.modality,
+        referralCount: 0,
+        amount: 0,
+        referralAmount: 0,
+      };
+      groupedByDoctor[doctorId].modality.push(modalityEntry);
+    }
+
+    modalityEntry.referralCount += 1;
+    modalityEntry.amount += totalAmount;
+    modalityEntry.referralAmount += referralAmount;
+  }
+
+  const result = Object.values(groupedByDoctor).map((doctor: any) => {
+    return {
+      name: doctor.name,
+      id: doctor.id,
+      modality: doctor.modality,
+    };
+  });
+
+  return result;
+}
+
 export function DoctorsAnalyticsComponent({
   data,
 }: {
   data: BookingDto[] | undefined;
+  startDate: string;
 }) {
-  function formatOutputToDto(output: BookingDto[]) {
-    if (!output) {
-      return;
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const [date, setDate] = React.useState<DateRange>({
+    from: null,
+    to: null,
+  });
+
+  const dataa = React.useMemo(() => {
+    if (data) {
+      formatOutputToDto(data, date);
     }
-
-    const groupedByDoctor: any = {};
-
-    for (const item of output) {
-      const doctorId = item.consultant;
-
-      if (!groupedByDoctor[doctorId]) {
-        groupedByDoctor[doctorId] = {
-          name: item.consultantName,
-          id: doctorId,
-          modality: [],
-        };
-      }
-
-      const referralAmount = item.referralAmount ? item.referralAmount : 0;
-      const totalAmount = item.totalAmount ? item.totalAmount : 0;
-
-      let modalityEntry = groupedByDoctor[doctorId].modality.find(
-        (m: any) => m.name === item.modality
-      );
-
-      if (!modalityEntry) {
-        modalityEntry = {
-          name: item.modality,
-          referralCount: 0,
-          amount: 0,
-          referralAmount: 0,
-        };
-        groupedByDoctor[doctorId].modality.push(modalityEntry);
-      }
-
-      modalityEntry.referralCount += 1;
-      modalityEntry.amount += totalAmount;
-      modalityEntry.referralAmount += referralAmount;
-    }
-
-    const result = Object.values(groupedByDoctor).map((doctor: any) => {
-      return {
-        name: doctor.name,
-        id: doctor.id,
-        modality: doctor.modality,
-      };
-    });
-
-    return result;
-  }
-
-  const dataa = React.useMemo(() => formatOutputToDto(data!), [data]);
+  }, [data, date]);
 
   function getModalityNames(data: Data) {
     const modalityNames: string[] = [];
     if (!data) {
       return [];
     }
-    console.log(data, "data");
     for (const doctor of data) {
       for (const modality of doctor.modality) {
         if (!modalityNames.includes(modality.name)) {
@@ -150,7 +156,6 @@ export function DoctorsAnalyticsComponent({
       header: "Name",
       cell: ({ row }) => row.original?.name,
       // @ts-ignore
-      // filterFn: "dateRangeFilter",
     },
     {
       accessorKey: "modality",
@@ -212,36 +217,6 @@ export function DoctorsAnalyticsComponent({
     },
   ];
 
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = React.useState({});
-
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(2023, 8, 20),
-    to: addDays(new Date(2023, 8, 20), 120),
-  });
-
-  const dateRangeFilter = (
-    row: any,
-    columnId: any,
-    filterValue: any,
-    addDaysFunction: any
-  ) => {
-    if (!filterValue || !filterValue.from || !filterValue.to) {
-      return true;
-    }
-    const rowValue = new Date(row.getValue(columnId));
-    return (
-      rowValue >= filterValue.from &&
-      rowValue <= addDaysFunction(filterValue.to, 1)
-    );
-  };
-
   React.useEffect(() => {
     if (date) {
       setColumnFilters([{ id: "createdAt", value: date }]);
@@ -259,10 +234,6 @@ export function DoctorsAnalyticsComponent({
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
     onRowSelectionChange: setRowSelection,
-    filterFns: {
-      dateRangeFilter: (row, columnId, filterValue) =>
-        dateRangeFilter(row, columnId, filterValue, addDays),
-    },
     state: {
       sorting,
       columnFilters,
